d9ad0c8b32a293fd18c9f96fcf9041ee1bab32e5
Ilya Sikirytski <ilyasikirytski@gmail.com>
2022-04-28 22:27:43 +0300

    Initial commit

---
+++ src/chain_of_responsibility_example/FileLogger.java
@@ -0,0 +1,13 @@
+package chain_of_responsibility_example;
+
+public class FileLogger extends Logger {
+
+    public FileLogger(int priority) {
+        super(priority);
+    }
+
+    @Override
+    public void write(String message) {
+        System.out.println("File: " + message);
+    }
+}
