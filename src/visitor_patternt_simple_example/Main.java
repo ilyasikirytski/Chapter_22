b80819321248122c901c78d59a557eb34931e64b
Ilya Sikirytski <ilyasikirytski@gmail.com>
2022-05-01 01:22:29 +0300

    Visitor_pattern_simple_example \ change call of visitor

--- src/visitor_patternt_simple_example/Main.java
+++ src/visitor_patternt_simple_example/Main.java
@@ -4,7 +4,10 @@ public class Main {
     public static void main(String[] args) {
 //        new BadVisitor();
         Visitor visitor = new GoodVisitor();
-        visitor.visit(new EngineElement());
-        visitor.visit(new BodyElement());
+        BodyElement bodyElement = new BodyElement();
+        EngineElement engineElement = new EngineElement();
+
+        bodyElement.accept(visitor);
+        engineElement.accept(visitor);
     }
 }
