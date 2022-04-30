0ebf6af6adf3627348f2b97e8dd85017b3c3ad1b
Ilya Sikirytski <ilyasikirytski@gmail.com>
2022-04-30 21:10:59 +0300

    Strategy_pattern_example2

---
+++ src/strategy_pattern_example/Main.java
@@ -0,0 +1,13 @@
+package strategy_pattern_example;
+
+public class Main {
+    public static void main(String[] args) {
+        StrategyClient strategyClient = new StrategyClient();
+
+        int[] arr = {0, 3, 8, 1, 4};
+//        new BubbleSort()
+//        new InsertingSort();
+        strategyClient.setStrategy(new InsertingSort());
+        strategyClient.executeStrategy(arr);
+    }
+}
---
+++ src/strategy_pattern_example/Sorting.java
@@ -0,0 +1,6 @@
+package strategy_pattern_example;
+
+//strategy
+public interface Sorting {
+    void sort(int[] arr);
+}
