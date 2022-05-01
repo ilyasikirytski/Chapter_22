afe9b3932fc110e02f0337f255af379ab804411d
Ilya Sikirytski <ilyasikirytski@gmail.com>
2022-05-02 00:34:57 +0300

    Strategy pattern example\ fixed sort method InsertionSort

--- src/strategy_pattern_example/InsertionSort.java
+++ src/strategy_pattern_example/InsertionSort.java
@@ -9,7 +9,7 @@ public class InsertionSort implements Sorting {
         System.out.println("before inserting sort: " + Arrays.toString(arr));
         for (int i = 1; i < arr.length; i++) {
             int j = i;
-            while (j - 1 > 0 && arr[j] < arr[j - 1]) {
+            while (j - 1 >= 0 && arr[j] < arr[j - 1]) {
                 int temp = arr[j];
                 arr[j] = arr[j - 1];
                 arr[j - 1] = temp;
--- src/strategy_pattern_example/Main.java
+++ src/strategy_pattern_example/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         StrategyClient strategyClient = new StrategyClient();
 
-        int[] arr = {0, 3, 8, 1, 4};
+        int[] arr = {8, 0, 3, 8, 1, 4};
 //        new BubbleSort()
 //        new InsertingSort();
         strategyClient.setStrategy(new InsertionSort());
